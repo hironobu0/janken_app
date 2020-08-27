4c0db4f402b947ef8cae256a869ae69b0c0fe9e8
hironobu0 <35990370+hironobu0@users.noreply.github.com>
2020-08-27 23:58:02 +0900

    Update character.js

--- js/character.js
+++ js/character.js
@@ -20,6 +20,10 @@ character_tag = [
     "#maou",
     '<img class="main_image" src="../img/fantasy_maou_devil.png" alt="">',
   ],
+  [
+    "#hatena",
+    '<img class="main_image" src="../img/animal_dance_cat.png" alt="">',
+  ],
 ];
 
 $(".battle_area").hide();
@@ -28,9 +32,33 @@ for (const value of character_tag) {
   $(value[0]).hover(
     function () {
       if ($("header").is("#my_side")) {
-        $(".my_character").append(value[1]);
+        if (value[0] != "#hatena") {
+          $(".my_character").append(value[1]);
+        }
+        $(value[0]).on("click", function () {
+          if (value[0] == "#hatena" && $("header").is("#my_side")) {
+            const value = Math.ceil(Math.random() * (character_tag.length - 1));
+            $(".my_character").append(character_tag[value][1]);
+          }
+
+          $("header").html("<h1>Select enemy character!</h1>");
+          $("header").attr("id", "enemy_side");
+        });
       } else if ($("header").is("#enemy_side")) {
-        $(".enemy_character").append(value[1]);
+        if (value[0] != "#hatena") {
+          $(".enemy_character").append(value[1]);
+        }
+
+        $(value[0]).on("click", function () {
+          if (value[0] == "#hatena" && $("header").is("#enemy_side")) {
+            const value = Math.ceil(Math.random() * (character_tag.length - 1));
+            $(".enemy_character").append(character_tag[value][1]);
+          }
+          $("header").html("<h1>Battle start!</h1>");
+          $("header").attr("id", "judgement");
+
+          $(".battle_area").show();
+        });
       }
     },
     function () {
@@ -41,98 +69,4 @@ for (const value of character_tag) {
       }
     }
   );
-
-  $(value[0]).on("click", function () {
-    if ($("header").is("#my_side")) {
-      // $(".my_character").append(value[1]);
-      $("header").html("<h1>Select enemy character!</h1>");
-      $("header").attr("id", "enemy_side");
-    } else if ($("header").is("#enemy_side")) {
-      // $(".enemy_character").append(value[1]);
-      $("header").html("<h1>Battle start!</h1>");
-      $("header").attr("id", "judgement");
-      if ($("header").is("#judgement")) {
-        $(".battle_area").toggle();
-      }
-    }
-  });
 }
-
-// // while (stage == 4) {
-
-// // }
-
-// if ($("header").is(".my_side")) {
-//   for (const value of character_tag) {
-//     $(value[0]).hover(
-//       function () {
-//         $(".my_character").append(value[1]);
-//       },
-//       function () {
-//         $(".my_character").find("img:last").remove();
-//       }
-//     );
-
-//     $(value[0]).on("click", function () {
-//       $(".my_character").append(value[1]);
-//       $("header").html("<h1>Select enemy character!</h1>");
-//       $("header").addClass("enemy_side");
-//     });
-//   }
-// } else {
-//   for (const value of character_tag) {
-//     $(value[0]).hover(
-//       function () {
-//         $(".enemy_character").append(value[1]);
-//       },
-//       function () {
-//         $(".enemy_character").find("img:last").remove();
-//       }
-//     );
-
-//     $(value[0]).on("click", function () {
-//       $(".enemy_character").append(value[1]);
-//     });
-//   }
-// }
-
-// for (const value of character_tag) {
-//   $(value[0]).hover(
-//     function () {
-//       $(".enemy_character").append(value[1]);
-//     },
-//     function () {
-//       $(".enemy_character").find("img:last").remove();
-//     }
-//   );
-
-//   $(value[0]).on("click", function () {
-//     $(".enemy_character").append(value[1]);
-//     $("header").html("<h1>Select enemy character!</h1>");
-//   });
-// }
-
-// for (const value of character_tag) {
-//   $(value[0]).hover(
-//     function () {
-//       $(".my_character").append(value[1]);
-//     },
-//     function () {
-//       $(".my_character").find("img:last").remove();
-//     }
-//   );
-
-//   $(value[0]).on("click", function () {
-//     $(".my_character").append(value[1]);
-//     $("header").html("<h1>Select enemy character!</h1>");
-//   });
-// }
-
-// $("#yuusha").hover(
-//   function () {
-//     $(".my_character").append($('<img src="../img/yuusya_game.png" alt="">'));
-//   },
-//   function () {
-//     $(".my_character").find("img:last").remove();
-//   }
-// );
